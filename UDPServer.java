c4cdf1577c9c5851a3a5813c8de0888c336239e2
Adriansopii <121805300+Adriansopii@users.noreply.github.com>
2023-12-02 21:28:12 +0100

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -1,62 +1,86 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.*;
 
-public class UdpServer {
-    public static void main(String[] args) throws IOException {
-        // 1. Të krijohet socket lidhja me server;
-        // Krijimi i soket lidhjes
-        DatagramSocket ServerSocket = new DatagramSocket(5000);
-        DatagramSocket ServerSocket2 = new DatagramSocket(5001);
+public class UDPServer {
 
-        // 4. Të jetë në gjendje të lexoje mesazhet që dërgohen nga klientët;
-        // Leximi i te dhenave
-        byte [] leximiDhena = new byte[1024];
+    private static final int BUFLEN = 512;
+    private static final int PORT = 8888;
 
-        // Krijimi i pranuesit te paketes
-        DatagramPacket pranuesiDhenave = new DatagramPacket(leximiDhena, leximiDhena.length);
+    private static final Set<String> firstClient = new HashSet<>();
+    private static final Map<String, InetSocketAddress> clientAddresses = new HashMap<>();
 
-        // 3. Të pranojë kërkesat e pajisjeve që dërgojnë request
-        ServerSocket.receive(pranuesiDhenave);
-        ServerSocket2.receive(pranuesiDhenave);
+    public static void main(String[] args) {
+        System.out.println("UDP Server");
 
-        // Extract i mesazhi
-        String mesazhi = new String(leximiDhena, 0, pranuesiDhenave.getLength());
-        String mesazhi2 = new String(leximiDhena, 0, pranuesiDhenave.getLength());
-        // Leximi/Shfaqja e mesazhit nga Klienti
-        System.out.println("Mesazhi klientit: " + mesazhi);
-        System.out.println("Mesazhi i klientit (pa priviligje): " + mesazhi2);
-        System.out.println("IP e klientit: " + pranuesiDhenave.getAddress());
-    }
-}
-class Server1 {
-    public static void main (String args[]) throws Exception {
-        System.out.println("Enter Port Number!!!");
-        Scanner input = new Scanner(System.in);
-        int SPort = input.nextInt();
-        DatagramSocket srvskt = new DatagramSocket(SPort);
-        byte[] data = new byte[1024];
-        System.out.println("Enter a full file name to save data to it? ");
-        String path = input.next();
-        System.out.println("file: " + path + "will be created");
-        FileOutputStream FOS = new FileOutputStream(path);
-        DatagramPacket srvpkt = new DatagramPacket(data,1024);
-        System.out.println("Listening to portL " + SPort);
-        int Packetcounter=0;
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(PORT);
 
-        while (true){
-            srvskt.receive(srvpkt);
-            Packetcounter++;
-            String words = new String (srvpkt.getData());
-            InetAdress ip = srvpkt.getAddress();
-            int port = srvpkt.getPort();
-            System.out.println("Packet #" + Packetcounter + "Recieved from Host/ port" + ip + "/" + port);
-            FOS.write(data);
-            //out 16.flush();
-            if(Packetcounter >=100)
-            break;
-        }
-        FOS.close();
-        System.out.println("Data has been written to the file!")
-            }
-}
+            byte[] receiveData = new byte[BUFLEN];
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                System.out.println("Waiting for data...");
+                serverSocket.receive(receivePacket);
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                InetSocketAddress clientAddr = new InetSocketAddress(clientAddress, clientPort);
+
+                String clientKey = clientAddress.getHostAddress() + ":" + clientPort;
+
+                String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+                if (message.equals("execute")) {
+                    if (firstClient.isEmpty() || firstClient.contains(clientKey)) {
+                        int magicNumber = (int) (Math.random() * 100) + 1;
+
+                        String response = "Magic number generated. Try to guess! You have 5 guesses";
+                        sendToClient(serverSocket, response, clientAddr);
+
+                        int i = 5;
+                        while (i > 0) {
+                            i--;
+                            String guessMessage = receiveFromClient(serverSocket, clientAddr);
+
+                            int clientGuess = Integer.parseInt(guessMessage);
+
+                            if (clientGuess == magicNumber) {
+                                String successMessage = "Congratulations! You guessed the magic number!";
+                                sendToClient(serverSocket, successMessage, clientAddr);
+                                System.out.println("Client guessed the correct number!");
+                                break;
+                            } else if (clientGuess > magicNumber && i != 0) {
+                                String errorMessage = "The magic number is smaller! Your tries: " + i;
+                                sendToClient(serverSocket, errorMessage, clientAddr);
+                            } else if (clientGuess < magicNumber && i != 0) {
+                                String errorMessage = "The magic number is bigger! Your tries: " + i;
+                                sendToClient(serverSocket, errorMessage, clientAddr);
+                            }
+                            if (i == 0) {
+                                String errorMessage = "You lost the game! The magic number was: " + magicNumber;
+                                sendToClient(serverSocket, errorMessage, clientAddr);
+                                System.out.println("Client didn't guess the correct number");
+                            }
+                        }
+                        firstClient.add(clientKey);
+                    } else {
+                        String errorMsg = "Magic Number Game not allowed for this client";
+                        sendToClient(serverSocket, errorMsg, clientAddr);
+                    }
+                } else if (message.startsWith("file r: ")) {
+                    String filename = message.substring(8);
+                    sendFileContent(serverSocket, filename, clientAddr);
+                } else if (message.startsWith("file a: ")) {
+                    if (firstClient.isEmpty() || firstClient.contains(clientKey)) {
+                        String[] parts = message.substring(8).split(" ", 2);
+                        String filename = parts[0];
+                        String content = parts[1];
+                        appendFile(filename, content);
+                        String successMsg = "Content appended to file: " + filename;
+                        sendToClient(serverSocket, successMsg, clientAddr);
+                        firstClient.add(clientKey);
+                    } else {
+                        String errorMsg = "Append operation not allowed for this client";
+                        sendToClient(serverSocket, errorMsg, clientAddr);
+                    }
