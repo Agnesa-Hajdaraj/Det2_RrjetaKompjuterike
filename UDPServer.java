1123a0b3925a2d56fc79a0123c18b410f3680fc4
Adriatik Krasniqi <119697153+adriatik7@users.noreply.github.com>
2023-12-02 21:35:10 +0100

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -84,3 +84,78 @@ public class UDPServer {
                         String errorMsg = "Append operation not allowed for this client";
                         sendToClient(serverSocket, errorMsg, clientAddr);
                     }
+} else if (message.startsWith("file w: ")) {
+                    if (firstClient.isEmpty() || firstClient.contains(clientKey)) {
+                        String[] parts = message.substring(8).split(" ", 2);
+                        String filename = parts[0];
+                        String content = parts[1];
+                        writeFile(filename, content);
+                        String successMsg = "Content written to file: " + filename;
+                        sendToClient(serverSocket, successMsg, clientAddr);
+                        firstClient.add(clientKey);
+                    } else {
+                        String errorMsg = "Write operation not allowed for this client";
+                        sendToClient(serverSocket, errorMsg, clientAddr);
+                    }
+                } else {
+                    System.out.println("Client says: " + message);
+
+                    System.out.print("Enter message to client: ");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    String serverMessage = br.readLine();
+
+                    sendToClient(serverSocket, serverMessage, clientAddr);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendToClient(DatagramSocket socket, String message, InetSocketAddress clientAddr)
+            throws IOException {
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddr);
+        socket.send(sendPacket);
+    }
+
+    private static String receiveFromClient(DatagramSocket socket, InetSocketAddress clientAddr) throws IOException {
+        byte[] receiveData = new byte[BUFLEN];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length, clientAddr);
+        socket.receive(receivePacket);
+        return new String(receivePacket.getData(), 0, receivePacket.getLength());
+    }
+
+    private static void sendFileContent(DatagramSocket socket, String filename, InetSocketAddress clientAddr)
+            throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            StringBuilder contentBuilder = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+            String fileContent = contentBuilder.toString();
+            sendToClient(socket, fileContent, clientAddr);
+        } catch (FileNotFoundException e) {
+            String errorMsg = "Error opening file: " + filename;
+            sendToClient(socket, errorMsg, clientAddr);
+        }
+    }
+
+    private static void appendFile(String filename, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.write(content);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error opening file: " + filename);
+        }
+    }
+
+    private static void writeFile(String filename, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+        } catch (IOException e) {
+            System.out.println("Error opening file: " + filename);
+        }
+    }
+}
